26c8b518f6cf7c97455f16c7f639887cdf3468fb
Atul singh <Atul singh@Atul>
2022-01-04 19:42:37 +0530

    added constant class

---
+++ src/com/hms/Constant.java
@@ -0,0 +1,11 @@
+package com.hms;
+
+public class Constant {
+	final static int SYSTEMEXIT = 4;
+	final static int UPDATEPATIENTEXIT = 8;
+	final static int UPDATEDOCTOREXIT = 5;
+	final static int UPDATEAPPOINTMENTEXIT = 4;
+	final static int DOCTOREXIT = 6;
+	final static int PATIENTEXIT = 6;
+	final static int APPOINTMENTEXIT = 6;
+}
